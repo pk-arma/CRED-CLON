613a00fc8c77336122df87ed66a56489c764939c
PANKAJ KUMAR VISHWAKARMA <92736385+VISHWAKARMAJ@users.noreply.github.com>
2022-03-15 01:38:53 +0530

    ok

--- src/compoment/common/Header/index.js
+++ src/compoment/common/Header/index.js
@@ -4,14 +4,21 @@ const Header = () => {
   return (
     <div className="mobile-menu-wrapper">
       <div className="flex max-width header">
+        <div className="only-mobile ">
+          <button class="hamburger" type="button">
+            <span class="hamburger-box">
+              <span class="hamburger-inner"></span>
+            </span>
+          </button>
+        </div>
         <img
           className="header-logo"
           src="	https://web-images.credcdn.in/_next/assets/images/home-page/cred-logo.png"
           alt="logo"
         />
-        <div className="only-mobile flex">
+        <div className="non-mobile flex">
           <div className="header-nav-item"> credit score check</div>
-          <div>credit card bill payment</div>
+          <div className="header-nav-item">credit card bill payment</div>
         </div>
       </div>
     </div>
